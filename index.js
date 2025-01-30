d94244c3f77641d0488d156b65c4b3d13bf624c1
caleb <cakpomughe@gmail.com>
2025-01-30 14:58:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({
+        email: "cakpomughe@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/Caleboc1"
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
